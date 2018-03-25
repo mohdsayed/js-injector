0078e97456aa74fac2ce7405d3eed7aeff438169
Sayed Taqui <sayedwp@gmail.com>
2018-03-25 21:15:50 +0530

    Calculate total days in project

--- ac.js
+++ ac.js
@@ -2,31 +2,85 @@
 
 	class ACExtenstion {
 		constructor() {
+			this.setElementProps();
+			this.billableMinutes = 0;
+			this.nonBillableMinutes = 0;
+
+			this.addCalculationWrapper();
+
+			$( '#calculate-button' ).on( 'click', () => {
+				this.setElementProps();
+				this.billableMinutes = 0;
+				this.nonBillableMinutes = 0;
+				this.addTotal();
+				this.addTotalCalculation();
+			} );
+
+			this.addTotal();
+		}
+
+		setElementProps() {
 			this.$timeGroups = $( '.tracking_objects_list_group' );
-			this.addTotal()
+			this.$actionBarRight = $( '.page_action_bar_right' );
+
+			this.$actionBarRight.css( 'display', 'flex' );
 		}
 
 		addTotal() {
 			const _this = this;
+
 			this.$timeGroups.each( function() {
-				let $group = $( this );
-				let $groupObjects = $group.find( '.tracking_objects_list_group_object' );
+				const $group = $( this );
+				const $groupObjects = $group.find( '.tracking_objects_list_group_object' );
 				let totalMinutes = 0;
 
 				$groupObjects.each( function() {
-					let $groupObject = $( this );
-			        let time = $groupObject.find( '.tracking_object_value' ).find( 'span' ).text();
+					const $groupObject = $( this );
+			        const time = $groupObject.find( '.tracking_object_value' ).find( 'span' ).text();
+			        const isBillable = $groupObjects.find( '.tracking_object_status' ).html() === 'Billable';
+
 					if ( time ) {
-						const timeInMinute = _this.convertToMinutes( time );
-						totalMinutes += timeInMinute;
+						const timeInMinutes = _this.constructor.convertToMinutes( time );
+						totalMinutes += timeInMinutes;
+
+						if ( isBillable ) {
+							_this.billableMinutes += timeInMinutes;
+						} else {
+							_this.nonBillableMinutes += timeInMinutes;
+						}
 					}
 				} );
 
-				$group.append( `<p><strong>Total: ${ _this.getTotalHours( totalMinutes ) } </strong></p>` );
+				if ( ! $group.find('.time-total-container').length ) {
+					$group.append( '<div class="time-total-container"></div>' );
+				}
+
+				$group.find( '.time-total-container' ).html( `<p><strong>Total: ${ _this.constructor.getTotalHours( totalMinutes ) } </strong></p>` );
 			} );
 		}
 
-		getTotalHours( minutes ) {
+		addCalculationWrapper() {
+			if ( this.$actionBarRight.length ) {
+				this.$actionBarRight.prepend( `<div id="calculation-wrapper" style="margin-top: -15px;"><div id="calculation-container"></div><p><button id="calculate-button">Calculate Again</button></p></div>` );
+			}
+		}
+
+		addTotalCalculation() {
+			const billableHours = this.billableMinutes ? this.constructor.getTotalDays( this.billableMinutes ) : '0:00';
+			const nonBillableHours = this.nonBillableMinutes ? this.constructor.getTotalDays( this.nonBillableMinutes ) : '0:00';
+			const totalTime = billableHours || nonBillableHours ? this.constructor.getTotalDays( this.billableMinutes + this.nonBillableMinutes ) : '0:00';
+			const container = $( '#calculation-container' );
+
+			if ( container.length ) {
+				container.html( `<div>
+					<p><strong>Billable Time: ${ billableHours }</strong><i>( d:h:m )</i></p>
+					<p><strong>Non Billable: ${ nonBillableHours }</strong><i>( d:h:m )</i></p>
+					<p><strong>Total Time: ${ totalTime }</strong><i>( d:h:m )</i></p>
+				</div>` );
+			}
+		}
+
+		static getTotalHours( minutes ) {
 			let totalHours = Math.floor( minutes/60 );
 			let totalMinutes = minutes % 60;
 
@@ -36,7 +90,21 @@
 			return totalHours + ':' + totalMinutes;
 		}
 
-		convertToMinutes( time ) {
+		static getTotalDays( minutes ) {
+			const oneDay = 8;
+			let hours = Math.floor( minutes/60 );
+			let totalDays = Math.floor( hours/oneDay );
+			let totalHours = hours % oneDay;
+			let totalMinutes = minutes % 60;
+
+			const outputDays = totalDays > 0 ? totalDays : '0' + totalDays;
+			const outputHours = totalHours > 9 ? totalHours : '0' + totalHours;
+			const outputMinutes = totalMinutes > 9 ? totalMinutes : '0' + totalMinutes;
+
+			return outputDays + ':' + outputHours + ':' + outputMinutes;
+		}
+
+		static convertToMinutes( time ) {
 			const timeSplit = time.split( ':' );
 			const hours = parseInt( timeSplit[0], 10 );
 			const minutes = parseInt( timeSplit[1], 10 );
@@ -46,7 +114,7 @@
 	}
 
 	setTimeout( () => {
-			new ACExtenstion();
+		new ACExtenstion();
 	}, 2000 );
 
 })( jQuery );
